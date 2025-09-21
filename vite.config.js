6cd6d636e4f532886fca39d689833da621d86000
saneesh <sanukyj@gmail.com>
2025-09-21 20:47:35 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+    alias: [{ find: "@", replacement: "/src" }],
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:5001",
+      '/api': {
+        target: 'http://localhost:5001',
         changeOrigin: true,
+        historyApiFallback: true,
       },
     },
-    historyApiFallback: true, // <- move it here
   },
 });
